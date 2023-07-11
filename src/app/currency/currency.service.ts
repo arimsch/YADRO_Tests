f4ea79b990d48eff8dec4ac68b078c382977deb4
Arina Matushkina <arina1998-v2013@yandex.ru>
2023-07-11 17:34:04 +0300

    finish all

--- src/app/currency/currency.service.ts
+++ src/app/currency/currency.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { CurrencyQuotes } from './models/CurrencyQuotes';
+import { CurrencyApiService } from './currency-api.services';
 
-@Injectable({
-  providedIn: 'root',
-})
+const EMPTY_QUOTES: CurrencyQuotes = {
+  RUBCNY: 0,
+  RUBEUR: 0,
+  RUBGBP: 0,
+  RUBJPY: 0,
+  RUBTRY: 0,
+  RUBUSD: 0,
+};
+
+@Injectable()
 export class CurrencyService {
-  constructor() {}
+  public currencies$: BehaviorSubject<CurrencyQuotes> = new BehaviorSubject(
+    EMPTY_QUOTES
+  );
+
+  constructor(private readonly currencyApiServices: CurrencyApiService) {}
+
+  public fetchCourse(): void {
+    this.currencyApiServices
+      .getCourse()
+      .subscribe(data => this.currencies$.next(data.quotes));
+  }
 }
